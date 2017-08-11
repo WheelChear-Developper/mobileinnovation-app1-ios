9b59a5140f889a89ac534107201bd3ee71dfcd04
M.Amatani <amatani@mobile-innovation.jp>
2017-08-11 17:17:53 +0900

    Initial Commit

---
+++ mobileinnovation-app1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobileinnovation-app1
+//
+//  Created by M.Amatani on 2017/08/11.
+//  Copyright © 2017年 Mobile Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
