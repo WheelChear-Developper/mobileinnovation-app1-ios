fd255492d698177bc9a1b68cc8bfe607bd297a42
Wheelmasa <amatani@mobile-innovation.jp>
2017-08-17 17:11:51 +0900

    Navigation View　設定　 エラー修正

---
+++ mobileinnovation-app1/Views/Nav1_ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Nav1_ViewController.swift
+//  mobileinnovation-app1
+//
+//  Created by M.Amatani on 2017/08/17.
+//  Copyright © 2017年 Mobile Innovation. All rights reserved.
+//
+
+import UIKit
+
+class Nav1_ViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
