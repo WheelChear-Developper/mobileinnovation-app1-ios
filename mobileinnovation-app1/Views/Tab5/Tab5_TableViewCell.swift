c36ab9331881410cce0d06ed8807b7b208f464ee
Wheelmasa <amatani@mobile-innovation.jp>
2017-08-24 21:56:02 +0900

    設定画面設定

---
+++ mobileinnovation-app1/Views/Tab5/Tab5_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Tab5_TableViewCell.swift
+//  mobileinnovation-app1
+//
+//  Created by M.Amatani on 2017/08/24.
+//  Copyright © 2017年 Mobile Innovation. All rights reserved.
+//
+
+import UIKit
+
+class Tab5_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- mobileinnovation-app1/Views/Tab5/Tab5_ViewController.swift
+++ mobileinnovation-app1/Views/Tab5/Tab5_ViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class Tab5_ViewController: BaseViewController {
+class Tab5_ViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var settingTableview: UITableView!
+    var int_setting_boardTableRow: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // ステータスバー背景色設定
+        self.base_setStatusBarBackgroundColor(color: #colorLiteral(red: 0.2549019608, green: 0.3490196078, blue: 0.5411764706, alpha: 1))
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidDisappear(animated)
 
-        self.base_setStatusBarBackgroundColor(color: #colorLiteral(red: 0.2549019608, green: 0.3490196078, blue: 0.5411764706, alpha: 1))
+        // NavigationBarを非表示にしたい場合
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+
+        settingTableview.reloadData()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -29,14 +37,31 @@ class Tab5_ViewController: BaseViewController {
         super.didReceiveMemoryWarning()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    ///////////////////////////////////////////////// Table Method Groupe ////////////////////////////////////////////////////////////////
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if indexPath.row == 0 {
+
+            cell = tableView.dequeueReusableCell(withIdentifier: "Finger") as! Tab5_TableViewCell
+
+            return cell!
+        }
+
+        return cell!
+    }
+
+    @objc func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        // 行設定
+        int_setting_boardTableRow = indexPath.row
+
+        performSegue(withIdentifier: "infomatinSub",sender: nil)
+    }
+    ///////////////////////////////////////////////// Table Method Groupe ////////////////////////////////////////////////////////////////
 
 }
